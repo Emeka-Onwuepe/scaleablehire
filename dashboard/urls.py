96663d0a69419acc9d19fe5c4400c97c9da05aba
EMEKA ONWUEPE <PASCALEMY2010@GMAIL.COM>
2026-05-22 19:19:46 +0100

    Add email verification routes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('login/', views.AppLoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('verify-email/<str:uidb64>/<str:token>/', views.verify_email, name='verify_email'),
+    path('email-verification-sent/', views.email_verification_sent, name='email_verification_sent'),
+    path('resend-verification/', views.resend_verification_email, name='resend_verification'),
     path('idea/new/', views.submit_idea, name='submit_idea'),
     path('feedback/<int:idea_id>/<int:feedback_id>/', views.add_feedback, name='add_feedback'),
     path('delect_feedback/<int:feedback_id>/', views.delete_feedback, name='delete_feedback'),
     path('delect_idea/<int:idea_id>/', views.delete_idea, name='delete_idea'),
-
 
 ]
